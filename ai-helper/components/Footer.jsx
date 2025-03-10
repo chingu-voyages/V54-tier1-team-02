5a83590490d56d92d5d801f13378fe097b7e6eb1
Alison <alison@andersonholland.com>
2025-03-10 12:16:23 -0500

    Added Footer component and some initial styles

---
+++ ai-helper/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+
+function Footer() {
+
+    return (
+        <div className="footer">
+            <a href="https://github.com/chingu-voyages/V54-tier1-team-02" target="_blank">GitHub Repo</a>
+
+            <ul>
+                <li>JD, Product Owner</li>    
+            </ul>
+            <ul>
+                <li>Ronysha, Scrum Master</li>
+                <li>Chyna, Scrum Master</li>
+            </ul>
+            <ul className="dev">
+                <li>Brandi, Developer</li>
+                <li>Stephanie, Developer</li>
+                <li>Alison, Developer</li>
+                <li>Sarah, Developer</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Footer;
