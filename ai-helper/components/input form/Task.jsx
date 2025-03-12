a7e8ff6f04c76abe51f322927652dd624cb1d6c9
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-03-12 00:13:23 -0400

    Task input field initial commit

---
+++ ai-helper/components/input form/Task.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function TaskInput() {
+
+    return (
+        <label className="input">
+            Task: 
+            <textarea 
+                name="Task" 
+                defaultValue="'Provide a list of websites for organizations that provide programs and
+services which will help me transform what I've learned into experience that
+other job applicants will not have.'"
+                rows={5}
+                cols={80}              
+            />
+        </label>
+    );
+}
+
+export default TaskInput;
