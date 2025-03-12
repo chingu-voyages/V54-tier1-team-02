12a74689b0379bca96af8e873fabc471a937b811
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-03-12 10:49:11 -0400

    handleSubmit function defined

--- ai-helper/components/input form/InputContainer.jsx
+++ ai-helper/components/input form/InputContainer.jsx
@@ -5,10 +5,22 @@ import ConstraintInput from "./Constraint";
 import TaskInput from "./Task";
 import OutputInput from "./Output";
 
+
 function InputContainer() {
+    function handleSubmit(e) {
+        // Prevent the browser from reloading the page
+        e.preventDefault();
+
+        // Read the form data
+        const form = e.target;
+        const formData = new FormData(form);
+
+        // REPLACE API NAME! Also, this may not be how Gemini needs the form data sent.
+        fetch('/some-api', { method: form.method, body: formData});
+    }
 
     return (
-        <form>
+        <form method="post" onSubmit={handleSubmit} className="input-form">
                 <ul><PersonaInput/></ul>
                 <ul><ContextInput/></ul>
                 <ul><TaskInput/></ul>
