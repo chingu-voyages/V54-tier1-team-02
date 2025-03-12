5096f38d6c744068573928626ec916782985f3ce
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-03-11 23:53:58 -0400

    Removed imports from initial React install

--- ai-helper/src/App.jsx
+++ ai-helper/src/App.jsx
@@ -1,6 +1,4 @@
-import React, { useState, Component } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React, { Component } from "react";
 import "./App.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
