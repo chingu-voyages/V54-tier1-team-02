aff886acaef754caadea6ed306f9076d69935a69
Alison <alison@andersonholland.com>
2025-03-20 17:08:27 -0500

    Implemented layout and styles, not including error message

--- ai-helper/components/Model.jsx
+++ ai-helper/components/Model.jsx
@@ -3,11 +3,10 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 // dotenv.config();
 
-const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
+const genAI = new GoogleGenerativeAI(import.meta.env.VITE_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 export const generateContent = async (prompt) => {
   const result = await model.generateContent(prompt);
-  console.log(result.response.text());
   return result.response.text;
 };
