dab6040c3b6933078803be4434dd39fe9dec24de
brandi <branic1578@gmail.com>
2025-03-30 13:18:02 -0400

    Fixed the UI shift when the error message appears

--- ai-helper/components/InputForm.jsx
+++ ai-helper/components/InputForm.jsx
@@ -34,7 +34,6 @@ function InputContainer() {
       ...prevErrors,
       [name]: value.trim() ? "" : "Enter text in field",
     }));
-
   };
 
   //Variable to clear a section
@@ -171,7 +170,7 @@ function InputContainer() {
             <div className="form-item-heading-area">
               <label htmlFor="context">Context</label>
               <p className="note">
-                Background information to get more control over the output
+                Background information to for control over the output
               </p>
             </div>
             <div id="context">
